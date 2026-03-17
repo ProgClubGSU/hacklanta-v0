a69c35c91aadd0130a3df7ff95a3bcd7a83cff3f
Poorav <pooravrawat@gmail.com>
2026-03-17 14:44:44 -0400

    Deadline added + fixed embed

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,11 +1,15 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server';
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server';
+// 
+// const isProtectedRoute = createRouteMatcher(['/apply(.*)', '/status(.*)']);
+// 
+// export const onRequest = clerkMiddleware((auth, context) => {
+//     const { isAuthenticated, redirectToSignIn } = auth();
+// 
+//     if (!isAuthenticated && isProtectedRoute(context.request)) {
+//         return redirectToSignIn();
+//     }
+// });
 
-const isProtectedRoute = createRouteMatcher(['/apply(.*)', '/status(.*)']);
-
-export const onRequest = clerkMiddleware((auth, context) => {
-    const { isAuthenticated, redirectToSignIn } = auth();
-
-    if (!isAuthenticated && isProtectedRoute(context.request)) {
-        return redirectToSignIn();
-    }
-});
+export const onRequest = async (context, next) => {
+  return next();
+};
